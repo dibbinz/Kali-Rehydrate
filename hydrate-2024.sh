7d074ca9de1cf87f419066cfe3b15081d670bf47
ITMustang <ITmustang@users.noreply.github.com>
2024-07-21 19:37:40 -0400

    Update hydrate-2024.sh
    rm nessus deb file after installation

--- hydrate-2024.sh
+++ hydrate-2024.sh
@@ -347,6 +347,7 @@ case $choice in
         curl --request GET --url 'https://www.tenable.com/downloads/api/v2/pages/nessus/files/Nessus-10.7.4-ubuntu1404_amd64.deb'
         --output 'Nessus-10.7.4-ubuntu1404_amd64.deb'
         dpkg -i Nessus-10.7.4-ubuntu1404_amd64.deb
+        rm Nessus-10.7.4-ubuntu1404_amd64.deb
       
         # ----- Install TeamViewer if not installed -----
         if ! command -v teamviewer &> /dev/null; then
